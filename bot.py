7db2a2eeaf4aaa6b2b5f5767baac84757712d941
1986sherik-max <1986sherik@gmail.com>
2026-04-26 02:30:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,8 @@ bot = telebot.TeleBot(TOKEN)
 films = {
     "1001": "BAACAgIAAxkBAAMHaezyG-4i53vQU5XIUtFVDzWErzcAAimeAALt42hLO2p8GZTsnkU7BA",
     "1002": "BAACAgIAAxkBAAMSaez2j76yTGGGLwyhSZEDbPXGJmsAAlSfAALt42hLG7niw1XbCaE7BA",
-     "1003": "BAACAgIAAxkBAAM0ae0FjsrU8ldfOWBOKdjx9ykGFuwAAqmiAALt42hLS_mAl8aIjKw7BA"
+    "1003": "BAACAgIAAxkBAAM0ae0FjsrU8ldfOWBOKdjx9ykGFuwAAqmiAALt42hLS_mAl8aIjKw7BA"
 }
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "🎬 Kino kodini yuboring")
@@ -20,7 +19,7 @@ def send_film(message):
     code = message.text.strip()
 
     if code in films:
-        bot.send_video(message.chat.id, films[code])
+        bot.send_video(message.chat.id, films[code]), supports_streaming=True)
     else:
         bot.send_message(message.chat.id, "❌ Bunday kino topilmadi")
 
