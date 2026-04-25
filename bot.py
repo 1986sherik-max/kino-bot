25288c7f8433abe92f438fb1be4ff48996d18412
1986sherik-max <1986sherik@gmail.com>
2026-04-26 02:52:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 films = {
-    "1001": "BAACAgIAAxkBAAMiae00S8YJ85OWAAE0bP4Tz-YKfp9tAAIpngAC7eNoSzqJJJsi-m4iOwQ"
+    "1001": "BAACAgIAAxkBAAMiae00S8YJ85OWAAE0bP4Tz-YKfp9tAAIpngAC7eNoSzqJJJsi-m4iOwQ",
+    "1002": "BAACAgIAAxkBAAM5ae0292fWNdi2exkHGTz4jMXv6CMAAlSfAALt42hLr8tJYe1cFsc7BA",
+    "1003": "BAACAgIAAxkBAAM7ae03T7Xc_4tLXDuAsDTLrffQZFgAAqmiAALt42hLh2oFoxNO8-o7BA"
 }
 
 @bot.message_handler(commands=['start'])
@@ -21,7 +23,4 @@ def send_film(message):
         bot.send_video(message.chat.id, films[code])
     else:
         bot.send_message(message.chat.id, "❌ Bunday kino topilmadi")
-@bot.message_handler(content_types=['video'])
-def get_id(message):
-    bot.send_message(message.chat.id, message.video.file_id)
 bot.infinity_polling()
