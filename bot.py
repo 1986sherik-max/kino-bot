702d9aa705e8670165365688199e248079489452
1986sherik-max <1986sherik@gmail.com>
2026-04-26 02:24:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 films = {
     "1001": "BAACAgIAAxkBAAMHaezyG-4i53vQU5XIUtFVDzWErzcAAimeAALt42hLO2p8GZTsnkU7BA",
     "1002": "BAACAgIAAxkBAAMSaez2j76yTGGGLwyhSZEDbPXGJmsAAlSfAALt42hLG7niw1XbCaE7BA",
-     "1003": "BAACAgIAAxkBAAMHaezyG-4i53vQU5XIUtFVDzWErzcAAimeAALt42hLO2p8GZTsnkU7BA"
+     "1003": "BAACAgIAAxkBAAM0ae0FjsrU8ldfOWBOKdjx9ykGFuwAAqmiAALt42hLS_mAl8aIjKw7BA"
 }
 
 @bot.message_handler(commands=['start'])
