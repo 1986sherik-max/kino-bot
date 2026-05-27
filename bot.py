b1ad3801a9292e8e12e621af7d2a11b70afd223f
1986sherik-max <1986sherik@gmail.com>
2026-05-27 21:47:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,32 +4,8 @@ TOKEN = "8778523615:AAHvknRCnbKmbvbb809HTQDW9n1AzKJJtH8"
 
 bot = telebot.TeleBot(TOKEN)
 
-movies = {
-    "1002": 3,
-    "1002": 26,
-}
 
-CHANNEL_ID = -1001234567890
-
-
-@bot.message_handler(func=lambda message: True)
-def send_movie(message):
-
-    code = message.text.strip()
-
-    if code in movies:
-
-        bot.forward_message(
-            chat_id=message.chat.id,
-            from_chat_id=CHANNEL_ID,
-            message_id=movies[code]
-        )
-
-    else:
-        bot.send_message(message.chat.id, "Kino topilmadi")
-
-
-@bot.message_handler(func=lambda message: True, content_types=['all'])
+@bot.message_handler(content_types=['all'])
 def test(message):
     print(message)
 
