1616efef435b6a48cd3f9516d7c4214be3e23597
1986sherik-max <1986sherik@gmail.com>
2026-05-28 12:46:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import telebot
 
-TOKEN = "8778523615:AAG_GafD8K-M6joaswuWM4i985sI1N_YP5s"
+TOKEN = "TOKENINGIZ"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+@bot.message_handler(func=lambda m: True, content_types=['any'])
 def test(message):
 
-    if message.forward_from_chat:
+    print(message)
+
+    if message.forward_origin:
         bot.reply_to(message, "FORWARD KELDI ✅")
     else:
         bot.reply_to(message, "Oddiy xabar")
