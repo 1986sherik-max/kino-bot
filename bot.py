d8bf6f41507f46b198e35684c765e2da14c5857b
1986sherik-max <1986sherik@gmail.com>
2026-05-28 12:41:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
-import os
 import telebot
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "8778523615:AAG_GafD8K-M6joaswuWM4i985sI1N_YP5s"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.reply_to(message, "Ishlayapman ✅")
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def test(message):
 
-print("Bot started")
+    if message.forward_from_chat:
+        bot.reply_to(message, "FORWARD KELDI ✅")
+    else:
+        bot.reply_to(message, "Oddiy xabar")
+
+print("Bot ishladi")
 
 bot.infinity_polling()
