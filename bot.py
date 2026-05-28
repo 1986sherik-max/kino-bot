fe02bab727643bf8aee527a7bea6829da1d488cc
1986sherik-max <1986sherik@gmail.com>
2026-05-28 13:11:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,20 +4,9 @@ TOKEN = "8778523615:AAG_GafD8K-M6joaswuWM4i985sI1N_YP5s"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(content_types=[
-    'text',
-    'video',
-    'photo',
-    'document',
-    'audio'
-])
+@bot.message_handler(func=lambda message: True)
 def test(message):
 
-    if message.forward_date:
-        bot.reply_to(message, "FORWARD KELDI ✅")
-    else:
-        bot.reply_to(message, "Oddiy xabar")
-
-print("Bot ishladi")
+    print(message)
 
 bot.infinity_polling(none_stop=True)
