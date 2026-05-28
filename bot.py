0d055d7faa08f252f1b7b0fc22f2d56419fd0af7
1986sherik-max <1986sherik@gmail.com>
2026-05-28 12:23:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,8 @@ TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
-bot.remove_webhook()
-
-@bot.message_handler(content_types=['all'])
+@bot.message_handler(func=lambda m: True)
 def test(message):
-
-    print("====== MESSAGE ======")
-
     print(message)
 
-    if message.forward_from_chat:
-        print("KANAL ID:")
-        print(message.forward_from_chat.id)
-
-bot.infinity_polling(skip_pending=True)
+bot.infinity_polling()
