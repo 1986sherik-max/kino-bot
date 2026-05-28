46309138da27ed12d7c0c806d4baf17771b5678e
1986sherik-max <1986sherik@gmail.com>
2026-05-28 13:01:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,13 @@ TOKEN = "8778523615:AAG_GafD8K-M6joaswuWM4i985sI1N_YP5s"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=[
+    'text',
+    'video',
+    'photo',
+    'document',
+    'audio'
+])
 def test(message):
 
     if message.forward_date:
